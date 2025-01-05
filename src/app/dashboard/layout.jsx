831c8bd91c0f55c6cdba5fcca48be6ed34c01ce2
rafi021 <mahmud.ibrahim021@gmail.com>
2025-01-05 22:41:18 +0600

    routes

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardLayout = ({ children }) => {
+    return (
+        <div>
+            <h1>DashboardLayout</h1>
+            <div>{children}</div>
+        </div>
+    )
+}
+
+export default DashboardLayout
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,27 @@
 
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        {children}
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link className="nav-link" href="/">Home</Link>
+              </li>
+              <li>
+                <Link className="nav-link" href="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>Footer</footer>
       </body>
     </html>
   );
