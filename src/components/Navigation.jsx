ebebc317681eddcb8ff44c702dc9940656c580f9
rafi021 <mahmud.ibrahim021@gmail.com>
2025-01-10 17:03:39 +0600

    auth & guest links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import NavLink from '@/components/NavLink';
-const Navigation = () => {
+import getAuthUser from '@/lib/getAuthUser';
+const Navigation = async () => {
+
+    const authUser = await getAuthUser();
+
+
     return (
         <nav>
             <ul>
                 <NavLink label="Home" href="/" />
-                <div className="flex justify-between">
-                    <NavLink label="Register" href="/register" />
-                    <NavLink label="Login" href="/login" />
-                    <NavLink label="Dashboard" href="/dashboard" />
-
-                </div>
+                {authUser ? (
+                    <div className="flex items-center">
+                        <NavLink label="Dashboard" href="/dashboard" />
+                    </div>
+                ) : (
+                    <div className="">
+                        <NavLink label="Register" href="/register" />
+                        <NavLink label="Login" href="/login" />
+                    </div>
+                )}
             </ul>
         </nav>
     )
---
+++ src/lib/getAuthUser.js
@@ -0,0 +1,11 @@
+import { cookies } from "next/headers";
+import { decrypt } from "./session";
+
+export default async function getAuthUser() {
+  const cookieStore = await cookies();
+  const session = cookieStore.get("session")?.value;
+  if (session) {
+    const user = await decrypt(session);
+    return user;
+  }
+}
