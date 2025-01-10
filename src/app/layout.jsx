f6c6eb69c1d44c16c123c541b8cbe67c1aa3b1d8
rafi021 <mahmud.ibrahim021@gmail.com>
2025-01-10 15:16:33 +0600

    added google fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,23 @@
 
 import Link from "next/link";
 import "./globals.css";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['200', '400', '700']
+});
+
+export const metadata = {
+  title: 'Next Project',
+  description: 'Page Description',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable} font-sans`}>
       <body className={`antialiased`}>
         <header>
           <nav>
